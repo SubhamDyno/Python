120d501455f1813cd3b75e1808793a93cbf37ebd
Subham <40828094+SubhamDyno@users.noreply.github.com>
2019-07-15 18:40:38 +0530

    Import OS Usage

---
+++ importOS.py
@@ -0,0 +1,13 @@
+#This is to show you import OS work in python
+
+import os
+
+os.getcwd() #To get the current directory
+
+os.listdir() #path) #You can give path inside to list out all directories. Not giving any path will show the list of diretories in CWD.
+
+os.chdir('C:\Test\Subham') #give the path in change directory
+
+os.rename('Source', 'Destination') #Rename the directory
+
+os.rmdir('C:\Test') #Remove a directory
